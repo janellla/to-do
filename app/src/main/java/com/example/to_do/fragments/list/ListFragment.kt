6df41d34c866c3019d037dc5121a295801291650
zhane <zhaneliyarb@gmail.com>
2023-01-05 21:08:26 +0600

    Initial commit

---
+++ app/src/main/java/com/example/to_do/fragments/list/ListFragment.kt
@@ -0,0 +1,32 @@
+package com.example.to_do.fragments.list
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.to_do.R
+
+ class ListFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+        view.flo
+
+        return view
+    }
+}
